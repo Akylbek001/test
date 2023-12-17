c1a11f9cc76de31cbb103ad09e5c4933076c4741
Akylbek001 <aliby.kz@mail.ru>
2023-12-17 15:38:39 +0600

    minor fixes

--- your_module_for_test_development/src/test/kotlin/tests/SimpleLoggingBaseRestTest.kt
+++
@@ -1,8 +0,0 @@
-package tests
-
-import inlines.*
-import org.testng.annotations.Listeners
-import rest.RestAssuredListener
-
-@Listeners(RestAssuredListener::class)
-abstract class SimpleLoggingBaseRestTest: BaseRestTest() {}
